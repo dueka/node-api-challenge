90f24e7cb05e4c320e5bbc1e9e8e83fa74a68122
dueka <ekanemd@live.com>
2020-01-17 09:42:46 +0100

    get function created in projectsRouter

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -9,4 +9,16 @@ const {
 } = require("../data/helpers/projectModel");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The users information could not be retreived"
+      });
+    });
+});
+
 module.exports = router;
